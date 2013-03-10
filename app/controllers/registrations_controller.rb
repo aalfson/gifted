a6f09a8b05334f8fe7921449e23a1f80118867a6
Aaron Alfson <aaron.alfson@gmail.com>
2013-03-10 13:24:03 -0400

    redirect bug fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,24 +1,24 @@
 #Registrations controller to handle Svpply authorization redirect after registration
 
 class RegistrationsController < Devise::RegistrationsController
- 
-  #before_filter :authenticate_user!
+
+  before_filter :check_for_user
  
   #continues registration after Svpply authorization
   def continue
-    current_user.svpplyCode = params[:code]
-    current_user.save
+      current_user.svpplyCode = params[:code]
+      current_user.save
   end
   
   #processes registration/continue form and redirects to success page
   def finish
-    u = current_user
-    u.name = params[:name]
+    
+    current_user.name = params[:name]
     
     year = params[:birthday]["(1i)"].to_i
     month = params[:birthday]["(2i)"].to_i
     day = params[:birthday]["(3i)"].to_i
-    u.birthday = Date.new(year, month, day)
+    current_user.birthday = Date.new(year, month, day)
     
     recipients = Array.new()
     
@@ -38,11 +38,17 @@ class RegistrationsController < Devise::RegistrationsController
       recipients = recipients.push(params[:recipient5].to_s)      
     end
     
-    u.recipients = recipients
-    u.save 
+    current_user.recipients = recipients
+    current_user.save 
   end
   
   protected
+  
+  def check_for_user
+    if user_signed_in? == false
+      redirect_to root_path
+    end
+  end
 
   def after_sign_up_path_for(resource)
     #specify Svpply o-auth login url here. 
