0ddf54fe9e61118c617ac302c9d42e3ba8902052
Aaron Alfson <aaron.alfson@gmail.com>
2013-03-10 20:29:45 -0400

    parsed Svpply json response and filtered for Zappos products

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,12 +16,28 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :name, :birthday, :recipients
   
-  def getSvpplyWants
+  def sendEmails
+    
+    zapposSvpplyWantsProducts = getZapposSvpplyWants
+    
+  end 
+  
+  #Pulls user's wants from Svpply. Returns only those products available for purchase at Zappos
+  def getZapposSvpplyWants
     
     svpplyUserId = self.svpplyUserId
     productsResponse = JSON.parse(HTTParty.get("https://api.svpply.com/v1/users/#{svpplyUserId}/wants/products.json").response.body)["response"]["products"]
-    puts productsResponse
     
+    zapposSvpplyWants = Array.new()
+    
+    #filter out products for sale at Zappos
+    productsResponse.each do |p|
+      if p["store"].to_s.include?("Zappos")
+        zapposSvpplyWants.push(p)
+      end
+    end
+    
+    zapposSvpplyWants
   end
 
 end
