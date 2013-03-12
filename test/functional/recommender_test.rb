4c3b0ae710796f05675313fb7c4d73da2e9b4b04
Aaron Alfson <aaron.alfson@gmail.com>
2013-03-11 23:13:59 -0400

    testing coverage update.

--- test/functional/recommender_test.rb
+++ test/functional/recommender_test.rb
@@ -2,10 +2,13 @@ require 'test_helper'
 
 class RecommenderTest < ActionMailer::TestCase
   test "sendEmail" do
-    mail = Recommender.sendEmail
-    assert_equal "Sendemail", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
+    
+    user = users(:peter).find
+    
+    mail = Recommender.sendEmail user, nil, user.email
+    assert_equal "#{user.name}'s Birthday Gift Ideas", mail.subject
+    assert_equal ["#{user.email}"], mail.to
+    assert_equal ["giftedDemoApp@gmail.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
 
