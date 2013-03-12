697ad699b104f503955ac3bc186083df308d45d9
Aaron Alfson <aaron.alfson@gmail.com>
2013-03-11 22:09:21 -0400

    rake bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,19 +32,19 @@ class User < ActiveRecord::Base
     products = getProducts
     
     
-    if user.recipient1 != nil or !user.recipient1.blank?
+    if user.recipient1 != nil and !user.recipient1.blank?
       Recommender.sendEmail(user, products, user.recipient1).deliver
     end
-    if user.recipient2 != nil or !user.recipient2.blank?
+    if user.recipient2 != nil and !user.recipient2.blank?
       Recommender.sendEmail(user, products, user.recipient2).deliver
     end
-    if user.recipient3 != nil or !user.recipient3.blank?
+    if user.recipient3 != nil and !user.recipient3.blank?
       Recommender.sendEmail(user, products, user.recipient3).deliver
     end
-    if user.recipient4 != nil or !user.recipient5.blank?
+    if user.recipient4 != nil and !user.recipient5.blank?
       Recommender.sendEmail(user, products, user.recipient4).deliver
     end
-    if user.recipient5 != nil or !user.recipient5.blank?
+    if user.recipient5 != nil and !user.recipient5.blank?
       Recommender.sendEmail(user, products, user.recipient5).deliver
     end  
     
